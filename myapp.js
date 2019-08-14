9f47a48c9903f578c2a6d9e7e57b3fb449358d41
yoann <yoann.renard@dedalus.eu>
2019-08-14 09:43:59 +0200

    add redirection to API

--- myapp.js
+++ myapp.js
@@ -14,11 +14,11 @@ collectDefaultMetrics({ prefix: 'my_application:' });
 // a custom histogram metric which represents the latency
 // of each call to our API /api/greeting.
 const histogram = new client.Histogram({
-  name: 'my_application:hello_duration',
-  help: 'Duration of HTTP requests in ms',
-  labelNames: ['method', 'status_code'],
-  buckets: [0.1, 5, 15, 50, 100, 500]
-});
+                                       name: 'my_application:hello_duration',
+                                       help: 'Duration of HTTP requests in ms',
+                                       labelNames: ['method', 'status_code'],
+                                       buckets: [0.1, 5, 15, 50, 100, 500]
+                                       });
 
 // create the express application
 const app = express();
@@ -28,19 +28,25 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // our API
 app.use('/api/greeting', (request, response) => {
-  // start the timer for our custom metric - this returns a function
-  // called later to stop the timer
-  const end = histogram.startTimer();
-  const name = request.query.name ? request.query.name : 'World';
-  response.send({content: `Hello, ${name}!`});
-  // stop the timer
-  end({ method: request.method, 'status_code': 200 });
-});
+        // start the timer for our custom metric - this returns a function
+        // called later to stop the timer
+        const end = histogram.startTimer();
+        const name = request.query.name ? request.query.name : 'World';
+        response.send({content: `Hello, ${name}!`});
+        // stop the timer
+        end({ method: request.method, 'status_code': 200 });
+        });
 
 // expose our metrics at the default URL for Prometheus
 app.get('/metrics', (request, response) => {
-  response.set('Content-Type', client.register.contentType);
-  response.send(client.register.metrics());
-});
+        response.set('Content-Type', client.register.contentType);
+        response.send(client.register.metrics());
+        });
+
+// redirect to our API
+app.get('/', (req,res) => {
+        res.redirect('/api/greeting')
+        });
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
